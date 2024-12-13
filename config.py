b579984d59e75707f9f538f5441a97d9715fbbb1
Kavya879 <agarwal.kavya1405@gmail.com>
2024-12-13 19:35:18 +0530

    sqlalchemy db push

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os
-
 class Config:
-    SECRET_KEY = os.urandom(24)  # For security
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///jobs.db'  # SQLite database
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
